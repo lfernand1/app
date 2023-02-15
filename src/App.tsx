00aab2656f5c61ead9467316e6b5e5364f9029e6
Larissa Fernanda <larifernand7@gmail.com>
2023-02-15 18:49:24 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from 'styled-components';
+import theme from 'assets/styles/theme';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
+        
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/types/orderType.ts
@@ -0,0 +1,5 @@
+export enum OrderType {
+    COMER_NO_LOCAL = 'Para agora',
+    PARA_VIAGEM = 'Para viagem',
+    DELIVERY = 'Delivery',
+}
