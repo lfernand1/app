f1c82f77ef3fb684e38ca9df0a02ce65db543191
Larissa Fernanda <larifernand7@gmail.com>
2023-02-16 23:23:32 -0300

    direto

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,6 @@
 import BoxLogin from "components/BoxLogin";
-import { LocalStorageHelper } from "helpers/LocalStorageHelper";
 import { useState } from "react";
-import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
-import { AuthService } from "services/AuthService";
-import { ErrorResponse } from "types/api/error";
-import { Login as LoginData, LoginResponse } from "types/api/login";
-import { User } from "types/api/user";
-import { LocalStorageKeys } from "types/LocalStorageKeys";
 import { RoutePath } from "types/routes";
 import * as S from "./style";
 
@@ -15,29 +8,8 @@ const Login = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
-    const mutation = useMutation(
-        AuthService.login, {
-        onSuccess: (data: LoginResponse & ErrorResponse) => {
-            if (data.statusCode) {
-                setErrorMessage(data.message)
-                return
-            }
-            if (data.token && data.user) {
-                LocalStorageHelper.set<string>(LocalStorageKeys.TOKEN, data.token)
-                LocalStorageHelper.set<User>(LocalStorageKeys.USER, data.user)
-                navigate(RoutePath.HOME);
-            }
-            setErrorMessage('Tente novamente!')
-
-        }, onError: () => {
-            setErrorMessage('Ocorreu um erro')
-        }
-    }
-);
-
-    const handleSubmit = (data: LoginData) => {
-        mutation.mutate(data)
-        setErrorMessage('');
+    const handleSubmit = () => {
+        navigate(RoutePath.HOME);
     }
 
     return (
--- src/router.tsx
+++ src/router.tsx
@@ -1,32 +1,27 @@
-import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
-import { RoutePath } from 'types/routes';
+import { Routes, Route } from "react-router-dom";
+import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
-import Login from 'pages/Login';
-import ManageProducts from 'components/ManageProducts';
-import ManageUsers from 'components/ManageUsers';
-import Settings from 'pages/Settings';
-import ManageTables from 'components/ManageTables';
-import { Auth } from 'helpers/Auth';
+import Login from "pages/Login";
+import ManageProducts from "components/ManageProducts";
+import ManageUsers from "components/ManageUsers";
+import Settings from "pages/Settings";
+import ManageTables from "components/ManageTables";
 
-
-const AuthenticatedRoutes = () => {
-    const isAuthenticated = Auth.isAuth();
-    return isAuthenticated ? <Outlet /> : <Navigate to={RoutePath.LOGIN} />
-}
 const Router = () => {
-    return (
-        <Routes>
-            <Route path={RoutePath.LOGIN} element={<Login />} />
-            <Route path="/" element={<AuthenticatedRoutes />}>
-                <Route path={RoutePath.HOME} element={<Home />} />
-                <Route path={RoutePath.SETTINGS} element={<Settings />}>
-                    <Route path={RoutePath.SETTINGS_TABLES} element={<ManageTables />} />
-                    <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
-                    <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
-                </Route>
-            </Route>
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path={RoutePath.LOGIN} element={<Login />} />
+      <Route path={RoutePath.HOME} element={<Home />} />
+      <Route path={RoutePath.SETTINGS} element={<Settings />}>
+        <Route path={RoutePath.SETTINGS_TABLES} element={<ManageTables />} />
+        <Route
+          path={RoutePath.SETTINGS_PRODUCTS}
+          element={<ManageProducts />}
+        />
+        <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default Router;
